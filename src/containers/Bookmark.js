a8a8df9bd08603a3926a0af080d3812d2918113a
javila35 <josepharthuravila@gmail.com>
2020-05-02 21:27:07 -0700

    Some progress on bookmark views.

--- src/containers/Bookmark.js
+++ src/containers/Bookmark.js
@@ -1,34 +1,61 @@
 import React, { Component } from 'react';
 import Comment from '../components/Comment';
 import { api } from '../services/api';
+import Loader from 'react-loading'
 
 
 // This component will show threaded comment and the bookmark.
 // Currently not being used anywhere.
 class Bookmark extends Component {
     state = {
-        bookmark: {
-            comments: []
-        }
+        bookmark: {},
+        comments: [],
+        loading: true
     };
 
     componentDidMount() {
-        this.getBookmarkDetails()
+        this.getBookmarkDetails();
     };
 
     getBookmarkDetails = () => {
-        let id =this.props.match.params.id
+        let id = this.props.match.params.id
         api.articles.getBookmark(id).then(data=>{
             console.log(data)
-            // this.setState({})
-        })
+            if (data.error) {
+                alert(data.error)
+                this.props.history.push('/')
+            } else {
+                console.log(data)
+                this.setState({
+                    bookmark: data.data.attributes,
+                    comments: [data.included],
+                    loading: false
+                })};
+        });
+    };
+
+    renderBookmark() {
+        const {article_img, article_link, article_title} = this.state.bookmark;
+        return(
+            <div id="bookmark-show">
+                <h1>{article_title}</h1>
+                <img src={article_img} alt={`${article_title} thumbnail`}></img>
+                <a href={article_link}>Read more.</a>
+            </div>
+        )
     };
 
     render() {
+        const {loading} = this.state.loading;
         return(
-            <div id="discussion">
-                <p>Lorem ipsum etc</p>
-            </div>
+            <>
+            {loading ? <Loader /> :
+                this.renderBookmark()
+            }
+                <div id="discussion">
+                    <p>Lorem ipsum etc</p>
+                </div>
+            </>
         )
     }
 };
