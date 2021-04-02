4c148a6b61dede4ee2cad67b1909fd52fdcecfdd
javila35 <josepharthuravila@gmail.com>
2021-04-02 13:57:13 -0700

    Update API on main

--- src/services/news_api.ts
+++ src/services/news_api.ts
@@ -37,14 +37,12 @@ export type TopArticlesResponseType = {
  * @param {string} query
  */
 const getArticles = (query: string) => {
-  // let url;
-  // if (query) {
-  //     url = ROOT_API + `search?q=${query}&${API_KEY}`;
-  // } else {
-  //     url = ROOT_API + "top-headlines?" + API_KEY;
-  // }
-  let url =
-    "https://newsapi.org/v2/top-headlines?country=us&apiKey=0a14a58999ab42358ae8ae02df4e0336";
+  let url;
+  if (query) {
+    url = ROOT_API + `search?q=${query}&${API_KEY}`;
+  } else {
+    url = ROOT_API + "top-headlines?" + API_KEY;
+  }
   return fetch(url).then((response) => response.json());
 };
 
