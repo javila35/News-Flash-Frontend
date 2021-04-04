e4f5c5d3add03c4c3e19763fff981f9a99ada905
javila35 <josepharthuravila@gmail.com>
2021-04-03 23:37:34 -0700

    Update API on main

--- src/services/news_api.ts
+++ src/services/news_api.ts
@@ -1,5 +1,5 @@
-// const ROOT_API = "https://gnews.io/api/v4/";
-// const API_KEY = `token=${process.env.REACT_APP_GNEWS_KEY}`;
+const ROOT_API = "https://gnews.io/api/v4/";
+const API_KEY = `token=${process.env.REACT_APP_GNEWS_KEY}`;
 
 /**
  * Query GNews API.
@@ -13,8 +13,8 @@ const getArticles = (query: string) => {
   } else {
     url = ROOT_API + "top-headlines?" + API_KEY;
   }
-  
-//   let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=0a14a58999ab42358ae8ae02df4e0336";
+
+  //   let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=0a14a58999ab42358ae8ae02df4e0336";
   return fetch(url).then((response) => response.json());
 };
 
