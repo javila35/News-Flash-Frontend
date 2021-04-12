5ad8a1294398e9bd2f7ab06ba8fc905d9c0cdf9e
javila35 <josepharthuravila@gmail.com>
2021-04-12 14:05:58 -0700

    Implement layout architecture Update todo list

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,22 @@
 import * as React from "react";
-import { Container } from "@material-ui/core";
+import { Container, makeStyles } from "@material-ui/core";
 import { AppBar } from "./AppBar";
 
+const classes = {
+  main: {
+    margin: "1em auto",
+  },
+};
+
+const useStyles = makeStyles(classes);
+
 export const Layout: React.FC = ({ children }) => {
+  const { main } = useStyles();
   return (
     <>
       <AppBar />
-      <Container disableGutters>
-          {/* Need to wrap the children in a React.Fragment to satisfy Material UI type */}
+      <Container disableGutters className={main}>
+        {/* Need to wrap the children in a React.Fragment to satisfy Material UI type */}
         <>{children}</>
       </Container>
     </>
